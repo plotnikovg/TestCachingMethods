a2e74f84d43f039312e36558c84d1217271c7590
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-04 20:21:42 +0300

    Add nullable to type

--- WithoutCaching/src/WithoutCachingDomain/Interfaces/IProductRepository.cs
+++ WithoutCaching/src/WithoutCachingDomain/Interfaces/IProductRepository.cs
@@ -5,5 +5,5 @@ namespace WithoutCachingDomain.Interfaces;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetProductsRangeByIdAsync(int firstId, int lastId);
-    Task<Product> GetProductByIdAsync(int id);
+    Task<Product?> GetProductByIdAsync(int id);
 }
