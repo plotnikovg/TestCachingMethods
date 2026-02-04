9090888795e9b5a9d291010758b170f13b5a630e
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-04 20:18:48 +0300

    Update methods name to async version

--- WithoutCaching/src/WithoutCachingDomain/Interfaces/IProductRepository.cs
+++ WithoutCaching/src/WithoutCachingDomain/Interfaces/IProductRepository.cs
@@ -4,6 +4,6 @@ namespace WithoutCachingDomain.Interfaces;
 
 public interface IProductRepository
 {
-    Task<IEnumerable<Product>> GetProductsRangeById(int firstId, int lastId);
-    Task<Product> GetProductById(int id);
+    Task<IEnumerable<Product>> GetProductsRangeByIdAsync(int firstId, int lastId);
+    Task<Product> GetProductByIdAsync(int id);
 }
