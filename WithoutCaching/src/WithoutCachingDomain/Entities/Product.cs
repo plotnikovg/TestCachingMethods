c77ed3c7818c03f2361c61f499982531569330c6
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-04 09:58:12 +0300

    Add CategoryId

--- WithoutCaching/src/WithoutCachingDomain/Entities/Product.cs
+++ WithoutCaching/src/WithoutCachingDomain/Entities/Product.cs
@@ -3,6 +3,7 @@ namespace WithoutCachingDomain.Entities;
 public class Product
 {
     public int Id { get; set; }
+    public int CategoryId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
