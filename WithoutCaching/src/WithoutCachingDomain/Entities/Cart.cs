c12ce072c03be1789141f61d5567f59416fcf9bc
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-04 12:01:57 +0300

    Add the "Add/Remove product" methods

--- WithoutCaching/src/WithoutCachingDomain/Entities/Cart.cs
+++ WithoutCaching/src/WithoutCachingDomain/Entities/Cart.cs
@@ -3,5 +3,27 @@ namespace WithoutCachingDomain.Entities;
 public class Cart
 {
     public int Id { get; set; }
-    public List<Product> Products { get; private set; } = new List<Product>();
+    private List<Product> _products = new List<Product>();
+    public IReadOnlyList<Product> Products => _products.AsReadOnly();
+
+    public void AddProduct(Product product)
+    {
+        if (product == null)
+            throw new ArgumentNullException(nameof(product));
+        
+        if (!product.IsInStock)
+            throw new InvalidOperationException("Product is not in the stock");
+        
+        if (_products.Contains(product))
+            throw new InvalidOperationException("Product is already in the cart");
+        
+        _products.Add(product);
+    }
+    
+    public void RemoveProduct(int productId)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == productId);
+        if (product != null)
+            _products.Remove(product);
+    }
 }
