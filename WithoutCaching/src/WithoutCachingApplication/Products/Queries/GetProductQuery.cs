67dcde6d9ae5c0bdef67252e011954359af7afe8
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-05 20:57:00 +0300

    Init

---
+++ WithoutCaching/src/WithoutCachingApplication/Products/Queries/GetProductQuery.cs
@@ -0,0 +1,38 @@
+using MediatR;
+using MediatR.Pipeline;
+using WithoutCachingApplication.Products.DTOs;
+using WithoutCachingDomain.Interfaces;
+
+namespace WithoutCachingApplication.Products.Queries;
+
+public sealed record GetProductQuery : IRequest<ProductDto>
+{
+    public int ProductId { get; init; }
+}
+
+public class GetProductQueryHandler : IRequestHandler<GetProductQuery, ProductDto>
+{
+    private readonly IProductRepository _productRepository;
+    
+    public GetProductQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<ProductDto> Handle(GetProductQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetProductByIdAsync(request.ProductId);
+
+        if (product is null) throw new NullReferenceException("Product not found");
+
+        return new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            CategoryId = product.CategoryId,
+            Description = product.Description,
+            Price = product.Price,
+            IsInStock = product.IsInStock
+        };
+    }
+}
---
+++ WithoutCaching/src/WithoutCachingApplication/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,44 @@
+using MediatR;
+using WithoutCachingApplication.Products.DTOs;
+using WithoutCachingDomain.Interfaces;
+
+namespace WithoutCachingApplication.Products.Queries;
+
+public sealed record GetProductsQuery : IRequest<List<ProductForListDto>>
+{
+    public int CategoryId { get; init; }
+    public int FirstProductNumber { get; init; }
+    public int LastProductNumber { get; init; }
+}
+
+public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, List<ProductForListDto>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductsQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<List<ProductForListDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products =
+            await _productRepository
+                .GetProductsRangeByIdAsync(request.CategoryId, request.FirstProductNumber, request.LastProductNumber);
+        
+        var productsDto = new List<ProductForListDto>();
+
+        foreach (var product in products)
+        {
+            productsDto.Add(new ProductForListDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                IsInStock = product.IsInStock
+            });
+        }
+        
+        return productsDto;
+    }
+}
