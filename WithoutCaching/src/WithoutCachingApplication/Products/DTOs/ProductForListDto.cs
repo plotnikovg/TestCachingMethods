b89582ba94aa3c489f11f42eb143cfd7ee620ae5
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2026-02-05 17:25:36 +0300

    Init

---
+++ WithoutCaching/src/WithoutCachingApplication/Products/DTOs/ProductForListDto.cs
@@ -0,0 +1,9 @@
+namespace WithoutCachingApplication.Products.DTOs;
+
+public record ProductForListDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsInStock { get; set; }
+}
